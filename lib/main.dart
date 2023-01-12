96fdf30f699649c71c6765380ea0bea356b6d1c0
patiphan2545 <patiphan.w@kkumail.com>
2023-01-12 21:10:34 +0700

    Flutter-HW1

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
+        primarySwatch: Colors.yellow,
       ),
       home: const HomeScreen(),
     );
@@ -30,47 +30,47 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Group member information"),
+        title: const Text("ข้อมูลสมาชิกในกลุ่ม"),
       ),
       body: GridView.count(
         crossAxisCount: 2,
         children: [
           Card(
               context: context,
-              color: Colors.redAccent,
-              blocktext: "Jeerasak Phalee",
+              color: Color.fromARGB(255, 0, 195, 255),
+              blocktext: "Narathip Phiwphan",
               imgPath:
-                  "https://scontent.fbkk5-3.fna.fbcdn.net/v/t39.30808-6/275375730_3099245676997956_523350226965638113_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeEREjH4EwBlPmR5Fu4Bh1LjDgkN-USt2JkOCQ35RK3YmcMkgV4548zV4feENCMewxlEsXgJdlg59BGa46j5ujak&_nc_ohc=p9Ob8VsuaxUAX-QqR_v&_nc_ht=scontent.fbkk5-3.fna&oh=00_AfC-CC9JrUk8vSDwiW4p_Ylm5aiLmcZNpYstJC3663pI5w&oe=63C163D0",
-              nickname: "Jonus",
-              id: "ID : 633410009-7",
+                  "https://scontent.fbkk5-5.fna.fbcdn.net/v/t39.30808-6/311565947_1513888799055164_6416576729503128272_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFP81_JDyt40MOUCMc0McSy9VTSozpslF31VNKjOmyUXTugp8Q-41q5iu8K8xOOM6CSnNFpPysMuZSqxK5rhWc1&_nc_ohc=x6NxG5WJUMEAX9_Mr7o&_nc_ht=scontent.fbkk5-5.fna&oh=00_AfBJECtXAySfuptqaHBFeDxv0ts-2fMDinf4GCLKD0aTKA&oe=63C42E8D",
+              nickname: "Flukk",
+              id: "ID : 633410015-2",
+              about: "about : football, Movies, สังสรรค์"),
+          Card(
+              context: context,
+              color: Color.fromARGB(255, 255, 0, 170),
+              blocktext: "Nawasin Intem",
+              imgPath:
+                  "https://scontent.fbkk5-5.fna.fbcdn.net/v/t39.30808-6/322180316_2377445172411647_8999569951487089387_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFM36HblS33LPzYfboq5utUfYu4Veh7Ph99i7hV6Hs-H-7r_gTLYRPnzyZTOjkAdhTVyD-qDW8ChMzBnLYQ4ylf&_nc_ohc=v8reew-_zf0AX8M9A9_&_nc_ht=scontent.fbkk5-5.fna&oh=00_AfDaTC00zetjFBugfjcS5ZFbYIoqMrDQyYe-csvM5zzenw&oe=63C57000",
+              nickname: "Earth",
+              id: "ID : 633410016-0",
               about: "about : football, Movies, Play games"),
           Card(
               context: context,
-              color: Colors.blueAccent,
-              blocktext: "Nattawat Wilai",
+              color: Color.fromARGB(255, 0, 255, 55),
+              blocktext: "Teerapat Limrattanaphan",
               imgPath:
-                  "https://scontent.fbkk5-3.fna.fbcdn.net/v/t39.30808-6/283917183_1915172192012669_129191124400808929_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeHENM8iESsdESfAjgmCCCcP-8APmxhPsGL7wA-bGE-wYk-PI5kZ0VAxjlACAb2w75BI5hAnRr_cOnGNO2p_QSWX&_nc_ohc=MDovlkKLGbkAX_vM6pT&_nc_ht=scontent.fbkk5-3.fna&oh=00_AfCLQLNQqhSVc0xyAkG_rM-PgU6hwXmFzMdiBZAcGCelSg&oe=63C1BA44",
-              nickname: "Bom",
-              id: "ID : 633410013-6",
-              about: "about : football, Movies, Play games"),
+                  "https://scontent.fbkk5-7.fna.fbcdn.net/v/t39.30808-6/313397206_2482224365250007_1275879670641022606_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeF_fyQORXTXeCg39huC0gGRqgKkvQyEBkyqAqS9DIQGTFLz68KXZ8u9MPYb6kt8aHcc6dDg6LVuGiFkI1x5OdRP&_nc_ohc=MbDhRVj-rBMAX-uFTvj&tn=FksMHfb3dCTdzmkC&_nc_ht=scontent.fbkk5-7.fna&oh=00_AfAvP_9ug4x2KkPJ4861ymGVEBGLUUJjVu8hGZC6401tsw&oe=63C4C1DC",
+              nickname: "Airk",
+              id: "ID : 633410079-6",
+              about: "about : football, Movies, Play games, สังสรรค์"),
           Card(
               context: context,
-              color: Colors.greenAccent,
-              blocktext: "Tanakon Wanit",
+              color: Color.fromARGB(255, 147, 135, 255),
+              blocktext: "Kitiikon Thamakittiphan",
               imgPath:
-                  "https://scontent.fbkk5-6.fna.fbcdn.net/v/t39.30808-6/321438238_1245726009621386_359110140792862665_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_eui2=AeEll7MLAw_JuU3QMC4UabbHo02Z4PVmLWujTZng9WYta4JV_8b5ZH3pKds9BtPpiI5-YJngWKBfT3CrrAKoxcJV&_nc_ohc=Q9qWRZdNtM0AX_7PpAY&_nc_ht=scontent.fbkk5-6.fna&oh=00_AfBOzuIUT7Oyg_Z-CsTNBIRERlMVuOePqCRpX59Z5GsxRw&oe=63C250A9",
-              nickname: "Kate",
-              id: "ID : 633410014-4",
-              about: "about : football, Movies, Play games"),
-          Card(
-              context: context,
-              color: Colors.yellowAccent,
-              blocktext: "Anusit Kitudomsuk",
-              imgPath:
-                  "https://scontent.fbkk5-4.fna.fbcdn.net/v/t39.30808-6/321287211_848595556407400_7704011169296610361_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGcOUd32vReQPhhZokI9ZGFZW8qCRSKZ4dlbyoJFIpnh3ZmkB0TveTPJA2A3Nmrvyzg2DGZStPX_-YJwyiU4zTd&_nc_ohc=cZ7x6vdBqm0AX-fsh8k&_nc_ht=scontent.fbkk5-4.fna&oh=00_AfBCiqwuuxJqAosHZomWSlasHThXqiL9ToJrOCdkCFh_Pg&oe=63C2FFB6",
-              nickname: "Kanun",
-              id: "ID : 633410036-4",
-              about: "about : football, Movies, Play games"),
+                  "https://scontent.fbkk5-6.fna.fbcdn.net/v/t1.6435-9/66224926_440430226511216_3675841356485361664_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=ad2b24&_nc_eui2=AeECvrjWIQRnH4-kUKg7kdxpNL1zInGgtbU0vXMicaC1tUXQCVrhsk2p2W3zMovaoKK_dhWfxzecjMtzeZJLSdgO&_nc_ohc=yQ4ZpEyLCF4AX93ohH-&tn=FksMHfb3dCTdzmkC&_nc_ht=scontent.fbkk5-6.fna&oh=00_AfDq9_3Gn8HqhBJm0ijBqxOAe9VIx6ZV9HYJqAnomhMQPw&oe=63E79B66",
+              nickname: "Sin",
+              id: "ID : 633410127-1",
+              about: "about : football, Movies, Play games, exercise"),
         ],
       ),
     );
@@ -132,7 +132,7 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("My First Flutter App"),
+          title: const Text("ข้อมูล"),
         ),
         body: Center(
             child: Column(
